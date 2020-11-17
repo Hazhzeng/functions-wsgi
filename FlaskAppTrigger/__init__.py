c2e2a6a222185d8697fb3a028fd555965645b751
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2020-11-17 13:29:16 -0800

    Use new folder structure

--- FlaskAppTrigger/__init__.py
+++ FlaskAppTrigger/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 import azure.functions as func
-from ..FlaskApp.wsgi import application
+from FlaskApp.wsgi import application
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return func.WsgiMiddleware(application).handle(req, context)
